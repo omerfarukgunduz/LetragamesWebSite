1ed9b3b174a5ce5ab451255dfb684ccc3dbafd8a
Ömer Faruk Gündüz <omerfarukgunduz034@gmail.com>
2023-10-16 20:16:12 +0300

    ekibimiz,hakkımızda ve admin sayfaları tamamlandı.

--- Letra/Controllers/HakkimizdaController.cs
+++ Letra/Controllers/HakkimizdaController.cs
@@ -16,5 +16,21 @@ namespace Letra.Controllers
             var veriler = c.Hakkimizda.ToList();
             return View(veriler);
         }
+        
+
+        public ActionResult HakkimizdaGetir(int id)
+        {
+            var hak = c.Hakkimizda.Find(id);
+            return View("HakkimizdaGetir",hak);
+        }
+
+        public ActionResult HakkimizdaGuncelle(Hakkimizda a)
+        {
+            var hakmzd = c.Hakkimizda.Find(a.Id);
+            hakmzd.Aciklama = a.Aciklama;
+            hakmzd.LogoLink = a.LogoLink;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
