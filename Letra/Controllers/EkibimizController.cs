72497b45dc6f58bad8c6db36d31d6b3bf1f6a545
Ömer Faruk Gündüz <omerfarukgunduz034@gmail.com>
2023-10-16 17:29:40 +0300

    ekibimiz sayfası tamamlandı. üye ekleme,güncelleme,silme işlemleri tamamlandı.

--- Letra/Controllers/EkibimizController.cs
+++ Letra/Controllers/EkibimizController.cs
@@ -12,11 +12,55 @@ namespace Letra.Controllers
     {
         // GET: Ekibimiz
         Context c = new Context();
-
         public ActionResult Index()
         {
             var veriler = c.Ekibimiz.ToList();
             return View(veriler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniUyeEkle()
+        {
+        return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniUyeEkle(Ekibimiz e)
+
+        {
+            c.Ekibimiz.Add(e);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult UyeSil(int id)
+        {
+            var uye = c.Ekibimiz.Find(id);
+            c.Ekibimiz.Remove(uye);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeGetir(int id)
+        {
+            var uye = c.Ekibimiz.Find(id);
+            return View("UyeGetir",uye );
+        }
+
+        public ActionResult UyeGuncelle(Ekibimiz e)
+        {
+            var uye = c.Ekibimiz.Find(e.Id);
+            uye.UyeAdSoyad = e.UyeAdSoyad;
+            uye.UyePozisyon = e.UyePozisyon;
+            uye.UyeFotograf = e.UyeFotograf;
+            uye.UyeLinkedin = e.UyeLinkedin;
+            uye.UyeGithub = e.UyeGithub;
+            uye.UyeMail = e.UyeMail;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
