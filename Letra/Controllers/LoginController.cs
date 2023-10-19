1a3e02290da86c04af652ecf9783599fa48aa12d
Ömer Faruk Gündüz <omerfarukgunduz034@gmail.com>
2023-10-19 14:52:58 +0300

    Authorize ayarları yapıldı.

--- Letra/Controllers/LoginController.cs
+++ Letra/Controllers/LoginController.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Letra.Models;
 
 namespace Letra.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         // GET: Login
         Context c = new Context();
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin a)
+        {
+            var adminkullanici = c.Admin.FirstOrDefault(x => x.KullaniciAdi == a.KullaniciAdi && x.Sifre == a.Sifre);
+            if (adminkullanici != null)
+            {
+                FormsAuthentication.SetAuthCookie(adminkullanici.KullaniciAdi, false);
+                Session["KullaniciAdi"] = adminkullanici.KullaniciAdi.ToString();
+                return RedirectToAction("Index","Admin");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
--- Letra/Global.asax.cs
+++ Letra/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Letra
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new AuthorizeAttribute());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
